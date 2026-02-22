7134d545f8192b5b86c45391a5f0a961ec73f7b3
Holden Rizzo <holdennrizzo@gmail.com>
2026-02-22 11:20:27 -0500

    initial commit

---
+++ src/auth/discord/callback.js
@@ -0,0 +1,54 @@
+import axios from 'axios';
+import { use } from 'react';
+
+export default async function(request, response) {
+    const code = request.query.code;
+    if (!code) {
+        return response.status(400).send("Invalid or no code provided");
+    }
+
+    const data = new URLSearchParams({
+        client_id: process.env.DISCORD_CLIENT_ID,
+        client_secret: process.env.DISCORD_CLIENT_SECRET,
+        grant_type: 'authorization_code',
+        code,
+        redirect_uri: 'https://starrys-substation.vercel.app/auth/redirect',
+        scope: 'identify connections'
+    });
+
+    try {
+        const token_response = await axios.post(
+            'https://discord.com/api/oauth2/token',
+            data.toString(),
+            {
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                }
+            }
+        )
+
+        const access_token = token_response.data.access_token
+        const user_response = await axios.get('https://discord.com/api/users/@me', {
+            headers: {
+                Authorization: `Bearer ${access_token}`
+            }
+        });
+        const user = user_response.data;
+
+        const query = new URLSearchParams({
+            id: user.id,
+            username: user.username,
+            discriminator: user.discriminator,
+            avatar: user.avatar || '',
+        });
+
+        response.writeHead(302, {
+            Location: `/pages/dashboard.html?${query.toString()}`,
+        });
+
+        response.end();
+    } catch (error) {
+        console.error('OAuth error', error);
+        response.send(500).send('Authentication failed');
+    }
+};
---
+++ src/auth/discord/index.js
@@ -0,0 +1,13 @@
+export default function handler(request, response) {
+    const redirect_uri = encodeURIComponent('https://starrys-substation.vercel.app/auth/discord/callback');
+    const client_id = process.env.DISCORD_CLIENT_ID;
+    const scope = encodeURIComponent('identify connections');
+
+    const oauth_url = process.env.DISCORD_REDIRECT;
+
+    response.writeHead(302, {
+        Location: oauth_url
+    });
+
+    response.end();
+}

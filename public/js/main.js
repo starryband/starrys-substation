c2d327b2c7dc4a9339f2d1c939b741df2fcf14de
Bright <buttons821@gmail.com>
2026-02-22 17:01:02 -0500

    index.html fixes

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,13 @@
 async function fetch_server() {
-    const response = await fetch("/api/scpsl/index");
-    const data = await response.json();
-    const element = document.getElementById('active_players');
+    try {
+        const response = await fetch("/api/scpsl/index");
+        const data = await response.json();
+        const element = document.getElementById('active_players');
 
-    element.innerHTML = data.players ?? '0';
+        element.innerHTML = data.players ?? '0';
+    } catch (err) {
+        console.error('Frontend fetch error:', err);
+    }
 }
 
 fetch_server();

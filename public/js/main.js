90b5148e5c7b1afe6637d27ec72045fdbbb65f84
Bright <buttons821@gmail.com>
2026-02-22 17:00:14 -0500

    index.html fixes

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,6 @@
 async function fetch_server() {
     const response = await fetch("/api/scpsl/index");
-    const data = response.json();
+    const data = await response.json();
     const element = document.getElementById('active_players');
 
     element.innerHTML = data.players ?? '0';
